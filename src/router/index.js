3c82e6d70b8b588c4efeb139dc17eee4ad644aa0
qiyuan106579 <1009162323@qq.com>
2021-07-24 22:41:06 +0800

    课程表页面

--- src/router/index.js
+++ src/router/index.js
@@ -10,28 +10,32 @@ import UserAdmin from '../components/admin/UserAdmin'
 import App from '../App'
 import UsersTable from '../components/admin/UsersTable'
 import Power from '../components/view/Power'
-import Security from "../components/SecurityGuard";
+import Security from '../components/SecurityGuard'
 import Curriculum from '../components/teacher/Curriculum'
 
 Vue.use(Router)
 
 const routes = [
-  { path:"/",name:"Login",component: Login},
-  { path:"/Menu/:cid",name:"Menu",component: Menu},
-  { path:"/Login12306",name:"Login12306",component: Login12306},
-  { path:"/Reg12306",name:"Reg12306",component: Reg12306},
-  { path:"/Index",name:"Index",component: Index,
-    children:[
-      {path:'/UserAdmin',name:'UserAdmin',component:UserAdmin},
-      {path:'/UserTable',name:'UserTable',component:UsersTable},
-      {path:'/Curriculum',name:'Curriculum',component:Curriculum},
+  {path: '/', name: 'Login', component: Login},
+  {path: '/Menu/:cid', name: 'Menu', component: Menu},
+  {path: '/Login12306', name: 'Login12306', component: Login12306},
+  {path: '/Reg12306', name: 'Reg12306', component: Reg12306},
+  {
+    path: '/Index', name: 'Index', component: Index,
+    children: [
+      {path: '/UserAdmin', name: 'UserAdmin', component: UserAdmin},
+      {path: '/UserTable', name: 'UserTable', component: UsersTable},
+      {path: '/Curriculum', name: 'Curriculum', component: Curriculum},
 
+      {path: '/Security', name: 'Security', component: Security},
     ]
   },
   // { path:"/",name:"App",component: App},
   { path:"/AdminIndex",name:"AdminIndex",component: AdminIndex},
   { path:"/Power",name:"Power",component: Power},
   { path:"/Security",name:"Security",component: Security},
+  {path: '/AdminIndex', name: 'AdminIndex', component: AdminIndex},
+  {path: '/Power', name: 'Power', component: Power},
 
 ]
 
