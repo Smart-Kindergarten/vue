056a3d256d60a9e13dec858410d3eea36decce9b
ELKSG <87411107+ELKSG@users.noreply.github.com>
2021-07-24 20:57:26 +0800

    菜单跳转 1.0

--- src/components/view/Index.vue
+++ src/components/view/Index.vue
@@ -1,18 +1,18 @@
 <template xmlns:router="">
-    <div style="margin-top: 0px;">
-<!--      -->
-      <div>
+  <div style="margin-top: 0px;">
 
-<!--        {{User}}-->
-        <el-row :gutter="20">
-          <el-col :span="6"><div class="grid-content bg-purple">
+    <div>
+      <!--        {{User}}-->
+      <el-row :gutter="20">
+        <el-col :span="3">
+          <div class="grid-content bg-purple">
             <el-menu
               router :default-active="this.$route.path"
               default-active="2"
               class="el-menu-vertical-demo"
               @open="handleOpen"
               @close="handleClose">
-              <el-submenu index="index" v-for="(item,index) in menuData" :key="item.mepid">
+              <el-submenu v-for="(item,index) in menuData" :key="item.mepid" :index="item.mepid">
                 <template slot="title">
                   <i class="el-icon-location"></i>
                   <span>{{item.mename}}</span>
@@ -20,83 +20,85 @@
                 <el-menu-item-group>
                   <el-menu-item :index="subItem.URL" v-for="(subItem,subIndex) in menuTwoData"
                                 v-if="subItem.meid === item.mepid" :key="subIndex">
-                    {{subItem.mename}}
-                    </el-menu-item>
+                    <router-link to="subItem.meurl">{{subItem.mename}}</router-link>
+                  </el-menu-item>
                 </el-menu-item-group>
               </el-submenu>
-
             </el-menu>
-          </div></el-col>
-          <el-col :span="18"><div class="grid-content bg-purple">
+          </div>
+        </el-col>
+        <el-col :span="18">
+          <div class="grid-content bg-purple">
             <router-view></router-view>
-          </div></el-col>
-        </el-row>
-
-
-      </div>
-
+          </div>
+        </el-col>
+      </el-row>
 
 
     </div>
+
+
+  </div>
 
 </template>
 
 <script>
   import Menu from './Menu'
+
   export default {
     // 接受父组件的值
     // props: {
     //   inputName: String,
     //   required: true
     // },
-    data() {
+    data () {
       return {
         msg: '我是一个组件',
         // curpage:1,
         // pagesize :5,
         // tableData:[],
-        menuData:[],
-        menuTwoData:[],
-        User:this.$route.params.Userid
+        menuData: [],
+        menuTwoData: [],
+        User: this.$route.params.Userid
       }
     },
     name: 'Index',
-    components : {
-      Menu : Menu
+    components: {
+      Menu: Menu
     },
     // mounted:function(){
-            // this.MenuALL();//需要触发的函数
+    // this.MenuALL();//需要触发的函数
     //       },
-    methods:{
-      handleOpen :function (curPage) {
-          this.curpage = curPage;
+    methods: {
+      handleOpen: function (curPage) {
+        this.curpage = curPage
       },
-      handleClose :function (val) {
-        this.pagesize = val;
+      handleClose: function (val) {
+        this.pagesize = val
       },
-      MenuALL:function () {
-          this.$axios.get("admin/MenuAll",{
-            params:{
-              uaccount:this.User,
-            },
-          }).then(response=>{
-            for (let i = 0; i < response.data.length; i++) {
-              if (response.data[i].meid === 0) {
-                this.menuData.push(response.data[i]);
-              }else {
-                this.menuTwoData.push(response.data[i]);
-                console.log(response.data[i]);
-              }
+      MenuALL: function () {
+        this.$axios.get('admin/MenuAll', {
+          params: {
+            uaccount: this.User,
+          },
+        }).then(response => {
+          for (let i = 0; i < response.data.length; i++) {
+            if (response.data[i].meid === 0) {
+              this.menuData.push(response.data[i])
+            } else {
+              this.menuTwoData.push(response.data[i])
+              console.log(response.data[i])
             }
-          }).catch((response) => {
-            console.log(error)
-          });
-          // this.$router.replace('/Login12306')
+          }
+        }).catch((response) => {
+          console.log(error)
+        })
+        // this.$router.replace('/Login12306')
       }
 
     }
-  ,mounted() {
-      this.MenuALL();
+    , mounted () {
+      this.MenuALL()
     }
 
   }
@@ -112,19 +114,24 @@
   .el-col {
     border-radius: 4px;
   }
+
   .bg-purple-dark {
     background: #99a9bf;
   }
+
   .bg-purple {
     background: #d3dce6;
   }
+
   .bg-purple-light {
     background: #e5e9f2;
   }
+
   .grid-content {
     border-radius: 4px;
     min-height: 36px;
   }
+
   .row-bg {
     padding: 10px 0;
     background-color: #f9fafc;
