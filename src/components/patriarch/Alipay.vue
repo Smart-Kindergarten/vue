bcb0b9ed15dec4944483b30a54c8d462f0270ad7
1285723569xb <baiwenlei+19971209>
2021-07-26 09:16:48 +0800

    支付宝支付

---
+++ src/components/patriarch/Alipay.vue
@@ -0,0 +1,34 @@
+<template>
+  <!--  <el-button type="primary" icon="el-icon-platform-eleme" @click="pay">进行缴费</el-button>-->
+  <el-form :inline="true" class="demo-form-inline">
+    <el-form-item>
+      <el-button type="primary" @click="pay">进行缴费</el-button>
+    </el-form-item>
+  </el-form>
+</template>
+
+<script>
+  export default {
+    name: "Alipay",
+    methods: {
+      pay() {
+        this.$axios.post("alipay").then(response => {
+          document.querySelector('body').innerHTML = response;
+          const div = document.createElement('div');
+          div.innerHTML = response.data;
+          document.body.appendChild(div);
+          document.forms[0].setAttribute('target', '_blank');// 新建窗口页面
+          this.$router.go(0);
+          document.forms[0].submit();
+        }).catch(error => {
+          console.log(error)
+        });
+      },
+    }
+
+  }
+</script>
+
+<style scoped>
+
+</style>
