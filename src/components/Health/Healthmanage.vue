0431e65f67d0d218d0d4cee51102843d24f1c113
1131473932 <87411414+1131473932@users.noreply.github.com>
2021-07-26 17:55:12 +0800

    Merge branch 'master' of C:\ideal\vue with conflicts.

---
+++ src/components/Health/Healthmanage.vue
@@ -0,0 +1,78 @@
+<!--     <h1 style="font-family: 'Helvetica Neue',Helvetica,'PingFang SC',-->
+<!--     'Hiragino Sans GB','Microsoft YaHei','微软雅黑',Arial,sans-serif; margin-left: auto">体检管理</h1>-->
+<!--    <span style="margin-left: auto">班级名称:</span>-->
+<!--    <button style="margin-left: 500px">新增体检情况</button>-->
+
+<template>
+<el-main>
+  <el-button type="primary" style="margin-left: 600px">新增体检情况</el-button>
+
+  <el-table
+    :data="tableData"
+    border
+    style="width: 100%">
+
+    <el-table-column
+            fixed
+            prop="babyheid"
+            label="宝宝编号"
+            width="120">
+          </el-table-column>
+          <el-table-column
+            prop="babyname"
+            label="宝宝名称"
+            width="120">
+          </el-table-column>
+          <el-table-column
+            prop="height"
+            label="身高"
+            width="80">
+          </el-table-column>
+          <el-table-column
+            prop="weight"
+            label="体重"
+            width="80">
+          </el-table-column>
+          <el-table-column
+            prop="eyesight"
+            label="视力"
+            width="80">
+          </el-table-column>
+          <el-table-column
+            prop="temperature"
+            label="体温"
+            width="80">
+          </el-table-column>
+          <el-table-column
+            prop="healthcondition"
+            label="健康状况"
+            width="120">
+          </el-table-column>
+          <el-table-column
+            prop="checkuptime"
+            label="体检时间"
+            width="120">
+          </el-table-column>
+          <el-table-column
+            fixed="right"
+            label="操作"
+            width="80">
+            <template slot-scope="scope">
+              <el-button type="text" size="small">修改</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+</el-main>
+      </template>
+<script>
+ export default {
+   name:"Healthmanage"
+ }
+ function f () {
+
+ }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -13,38 +13,35 @@ import Power from '../components/view/Power'
 import Security from "../components/SecurityGuard";
 import Curriculum from '../components/teacher/Curriculum'
 import Health from '../components/patriarch/Health'
-import Alipay from "../components/patriarch/Alipay";
-import SendMessage from "../components/patriarch/SendMessage";
+import Healthmanage from '../components/Health/Healthmanage'
 
 Vue.use(Router)
 
 const routes = [
-  {path: "/", name: "Login", component: Login},
-  {path: "/Menu/:cid", name: "Menu", component: Menu},
-  {path: "/Login12306", name: "Login12306", component: Login12306},
-  {path: "/Reg12306", name: "Reg12306", component: Reg12306},
-  {
-    path: "/Index", name: "Index", component: Index,
-    children: [
-      {path: '/UserAdmin', name: 'UserAdmin', component: UserAdmin},
-      {path: '/UserTable', name: 'UserTable', component: UsersTable},
-      {path: '/Curriculum', name: 'Curriculum', component: Curriculum},
-      {path: '/Health', name: 'Health', component: Health},
-      {path: "/Security", name: "Security", component: Security},
-      {path: "/Alipay", name: "Alipay", component: Alipay},
-      {path: "/SendMessage", name: "SendMessage", component: SendMessage},
+  { path:"/",name:"Login",component: Login},
+  { path:"/Menu/:cid",name:"Menu",component: Menu},
+  { path:"/Login12306",name:"Login12306",component: Login12306},
+  { path:"/Reg12306",name:"Reg12306",component: Reg12306},
+  { path:"/Index",name:"Index",component: Index,
+    children:[
+      {path:'/UserAdmin',name:'UserAdmin',component:UserAdmin},
+      {path:'/UserTable',name:'UserTable',component:UsersTable},
+      {path:'/Curriculum',name:'Curriculum',component:Curriculum},
+      {path:'/Health',name:'Health',component:Health},
+      {path:'/Healthmanage',name:'Healthmanage',component:Healthmanage},
+      { path:"/Security",name:"Security",component: Security},
     ]
   },
   // { path:"/",name:"App",component: App},
-  {path: "/AdminIndex", name: "AdminIndex", component: AdminIndex},
-  {path: "/Power", name: "Power", component: Power},
+  { path:"/AdminIndex",name:"AdminIndex",component: AdminIndex},
+  { path:"/Power",name:"Power",component: Power},
 
 
 ]
 
 export default new Router({
-  mode: "history",
-  routes: routes,
+  mode : "history",
+  routes :routes,
 
 
 })
