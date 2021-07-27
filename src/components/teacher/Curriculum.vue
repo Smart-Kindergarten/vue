a9c7010e4b126a1b16643c8a2efe44807c5298ee
qiyuan106579 <1009162323@qq.com>
2021-07-27 14:56:19 +0800

    课程表以一周日期做分页

--- src/components/teacher/Curriculum.vue
+++ src/components/teacher/Curriculum.vue
@@ -26,13 +26,23 @@
           </tbody>
         </table>
       </div>
-      <el-button type="primary" icon="el-icon-arrow-left" style="float: left;margin-left: 20px">上一周</el-button>
-      <el-button type="primary" style="float: right;margin-right: 20px">下一周<i class="el-icon-arrow-right el-icon--right"></i></el-button>
+      <el-button
+        type="primary"
+        icon="el-icon-arrow-left"
+        style="float: left;margin-left: 20px"
+        @click="getWeek('LastWeek')">上一周
+      </el-button>
+      <el-button
+        type="primary"
+        style="float: right;margin-right: 20px"
+        @click="getWeek('NextWeek')">下一周<i class="el-icon-arrow-right el-icon--right"></i></el-button>
     </div>
   </div>
 </template>
 
 <script>
+let systemTime = ''
+
 export default {
   name: 'Curriculum',
   data () {
@@ -45,8 +55,14 @@ export default {
       tableShow: false
     }
   },
+
   mounted: function () {
-    this.$axios.get('teacher/selectCurrAll').then(response => {
+    this.addDate()
+    this.$axios.get('teacher/selectCurrAll', {
+      params: {
+        date: this.systemTime
+      }
+    }).then(response => {
       console.log(response.data.length)
       this.className = response.data[0].classId
       var currDate = response.data[0].currDate
@@ -95,8 +111,7 @@ export default {
   },
 
   methods: {
-
-    /**
+    /*
      * 数字转中文
      * @param {Number} num 需要转换的数字
      * @param {String} identifier 标识符
@@ -107,6 +122,70 @@ export default {
       return identifier === 'week' && (num === 0 || num === 7) ? '日' : character[num]
     },
 
+    //当前时间
+    addDate () {
+      let nowDate = new Date()
+      let date = {
+        year: nowDate.getFullYear(),
+        month: nowDate.getMonth() + 1,
+        date: nowDate.getDate()
+      }
+      this.systemTime = date.year + '-' + date.month + '-' + date.date
+      console.log(this.systemTime)
+    },
+
+    getWeek: function (string) {
+      this.$axios.get('teacher/getWeekDate', {
+        params: {
+          week: string
+        }
+      }).then(response => {
+        console.log(response.data.length)
+        this.className = response.data[0].classId
+        var currDate = response.data[0].currDate
+        var date = currDate.split(',')
+        this.startTime = date[0]
+        this.endTime = date[1]
+        this.classTableData = [
+          {
+            'monday': response.data[0].currMonday,
+            'tuesday': response.data[0].currTuesday,
+            'wednesday': response.data[0].currWednesday,
+            'thursday': response.data[0].currThursday,
+            'friday': response.data[0].currFriday,
+          },
+          {
+            'monday': response.data[1].currMonday,
+            'tuesday': response.data[1].currTuesday,
+            'wednesday': response.data[1].currWednesday,
+            'thursday': response.data[1].currThursday,
+            'friday': response.data[1].currFriday,
+          },
+          {
+            'monday': response.data[2].currMonday,
+            'tuesday': response.data[2].currTuesday,
+            'wednesday': response.data[2].currWednesday,
+            'thursday': response.data[2].currThursday,
+            'friday': response.data[2].currFriday,
+          },
+          {
+            'monday': response.data[3].currMonday,
+            'tuesday': response.data[3].currTuesday,
+            'wednesday': response.data[3].currWednesday,
+            'thursday': response.data[3].currThursday,
+            'friday': response.data[3].currFriday,
+          },
+          {
+            'monday': response.data[4].currMonday,
+            'tuesday': response.data[4].currTuesday,
+            'wednesday': response.data[4].currWednesday,
+            'thursday': response.data[4].currThursday,
+            'friday': response.data[4].currFriday,
+          }]
+      }).catch(error => {
+        console.log(error)
+      })
+    },
   }
 }
 </script>
