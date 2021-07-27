697c66301115bda2b8f47d38097277b2c5d705d0
ELKSG <87411107+ELKSG@users.noreply.github.com>
2021-07-27 12:10:34 +0800

    幼儿保健  郑伟1.0  动态保健

--- src/components/patriarch/Health.vue
+++ src/components/patriarch/Health.vue
@@ -57,6 +57,7 @@
     name: 'Health',
     data() {
       return {
+        User: this.$route.params.Userid,
         currentPage1: 5,
         currentPage2: 5,
         currentPage3: 5,
@@ -75,6 +76,7 @@
         this.$axios.get("Health/HealthAll",{
           params:{
             page:val,
+              uaccount: this.User,
           },
         }).then(response=>{
           console.log(response.data)
