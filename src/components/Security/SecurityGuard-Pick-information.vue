4134d34316c1f485fd5509610bd4bf3c09db6d4e
LWX <1596379120@qq.com>
2021-07-30 04:39:29 +0800

    Secrity0.0.9 2021/07/30

---
+++ src/components/Security/SecurityGuard-Pick-information.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>123123</div>
+</template>
+
+<script>
+    export default {
+        name: "SecurityGuard-Pick-information",
+        methods:{
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/components/Security/SecurityGuard-Pick.vue
+++ src/components/Security/SecurityGuard-Pick.vue
@@ -1,69 +1,78 @@
 <template>
-  <el-tabs v-model="editableTabsValue" type="card" @edit="handleTabsEdit">
+  <el-tabs v-model="editableTabsValue" type="card" @tab-remove="removeTab">
     <el-tab-pane
       v-for="(item, index) in editableTabs"
       :key="item.name"
       :label="item.title"
       :name="item.name"
       :closable= "item.closable"
+      :lazy="item.lazy"
     >
-      {{item.content}}
+      <SecurityGuardPickQuery v-if="item.content" @addTab="addTab"></SecurityGuardPickQuery>
+      <SecurityGuardPickinformation v-else></SecurityGuardPickinformation>
     </el-tab-pane>
   </el-tabs>
 </template>
 
 <script>
-  import SecurityGuardTest from "./SecurityGuard-test"
+  import SecurityGuardPickQuery from "./SecurityGuard-Pick-Query"
+  import SecurityGuardPickinformation from "./SecurityGuard-Pick-information";
     export default {
-        name: "SecurityGuard-Pick",
-        data(){
-          return{
-            editableTabsValue: '1',
-            editableTabs: [{
-              title: 'Tab 1',
-              name: '1',
-              closable: false,
-              content:"123"
-            },{
-              title: 'Tab 2',
-              name: '2',
-              closable: true,
-              content: SecurityGuardTest
-            }],
-          }
-        },
+      name: "SecurityGuard-Pick",
+      data(){
+        return{
+          editableTabsValue: 'Query',
+          editableTabs: [{
+            title: '宝宝接送查询',
+            name: 'Query',
+            closable: false,
+            content: true
+          }],
+        }
+      },
       methods: {
-        handleTabsEdit(targetName, action) {
-          if (action === 'add') {
-            let newTabName = ++this.tabIndex + '';
-            this.editableTabs.push({
-              title: 'New Tab',
-              name: newTabName,
-              content: 'New Tab content'
-            });
-            this.editableTabsValue = newTabName;
-          }
-          if (action === 'remove') {
-            let tabs = this.editableTabs;
-            let activeName = this.editableTabsValue;
-            if (activeName === targetName) {
-              tabs.forEach((tab, index) => {
-                if (tab.name === targetName) {
-                  let nextTab = tabs[index + 1] || tabs[index - 1];
-                  if (nextTab) {
-                    activeName = nextTab.name;
-                  }
+        removeTab(targetName) {
+          let tabs = this.editableTabs;
+          let activeName = this.editableTabsValue;
+          if (activeName === targetName) {
+            tabs.forEach((tab, index) => {
+              if (tab.name === targetName) {
+                let nextTab = tabs[index + 1] || tabs[index - 1];
+                if (nextTab) {
+                  activeName = nextTab.name;
                 }
-              });
-            }
-
-            this.editableTabsValue = activeName;
-            this.editableTabs = tabs.filter(tab => tab.name !== targetName);
+              }
+            });
           }
+
+          this.editableTabsValue = activeName;
+          this.editableTabs = tabs.filter(tab => tab.name !== targetName);
+        },
+        addTab(baby) {
+          console.log(baby)
+          console.log(this.editableTabs)
+          for(var i of this.editableTabs){
+            if(i.name==baby[0].toString()){
+              this.editableTabsValue = baby[0].toString();
+              return
+            }
+          }
+          this.editableTabs.push({
+            title: baby[1],
+            name: baby[0].toString(),
+            closable: true,
+            babyID:baby[0],
+            content: false
+          });
+          this.editableTabsValue = baby[0].toString();
         }
       },
       components: {
-          SecurityGuardTest
+        SecurityGuardPickQuery,
+        SecurityGuardPickinformation
+      },
+      mounted() {
+
       }
     }
 </script>
