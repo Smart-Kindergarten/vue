3c28f97373918a69a1f8bc7764a63137b972cbe9
qiyuan106579 <1009162323@qq.com>
2021-08-07 23:32:33 +0800

    8.7修改发布作业

---
+++ src/components/teacher/ClassPhoto.vue
@@ -0,0 +1,19 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: 'ClassPhoto',
+  data () {
+    return {}
+  },
+  mounted () {
+  },
+  methods: {},
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -66,6 +66,8 @@ const routes = [
 
 export default new Router({
   mode: 'history',
+
+
   routes: routes,
 
 })
